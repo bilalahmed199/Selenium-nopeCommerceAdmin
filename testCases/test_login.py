9c7eaecc56c5fd9565773dfc19d888a6892afcfc
Bilal Ahmed <bilalriasat199@gmail.com>
2023-04-11 09:29:44 +0500

    testing git push

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -29,6 +29,7 @@ class Test_001_Login:
         act_title = self.driver.title
         self.driver.close()
 
+# testing git push
         if act_title == "Dashboard / nopCommerce administration":
             assert True
         else:
