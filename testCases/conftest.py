030b39760c179c7767c1afe612c85ff7fe2e0f63
Bilal Ahmed <bilalriasat199@gmail.com>
2023-04-12 13:19:20 +0500

    .

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -30,10 +30,11 @@ def pytest_configure(config):
     config._metadata['Project Name'] = 'nop Commerce'
     config._metadata['Module Name'] = 'Customers'
     config._metadata['Tester'] = 'Bilal'
+    
 
 # it is hook to delete/modify environment info to HTML report
-# @pytest.mark.optionalhook
-# def pytest_metadata(_metadata):
-#     # below info is deleted explicitly to not be shown in the HTML report
-#     metadata.pop("JAVA_Home", None)
-#     metadata.pop("Plugins", None)
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    # below info is deleted explicitly to not be shown in the HTML report
+    metadata.pop("JAVA_Home", None)
+    metadata.pop("Plugins", None)
