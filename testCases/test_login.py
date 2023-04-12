444bd2207bb11d5c7d3b3442039ef2f940e5dff2
Bilal Ahmed <bilalriasat199@gmail.com>
2023-04-12 13:49:44 +0500

    .

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -18,10 +18,11 @@ class Test_001_Login:
 
         act_title = self.driver.title
         # it will fail as wrong title given, screenshot will be created
-        if act_title == "wrong title Your store. Login":
-            assert True
+        if act_title == "Your store. Login":
             self.driver.close()
             self.logger.info("****** Login page loaded successfuly ******")
+            assert True
+
         else:
             self.driver.save_screenshot(".\\Screenshots\\" + "test_homePageLoaded.png")
             self.driver.close()
@@ -29,26 +30,26 @@ class Test_001_Login:
             assert False
 
    
-    def test_Login(self, setup ):
-        self.logger.info("****** Test_001_Login******")
-        self.logger.info("****** Verifying user Loggedin with valid data or not ******")
+    # def test_Login(self, setup ):
+    #     self.logger.info("****** Test_001_Login******")
+    #     self.logger.info("****** Verifying user Loggedin with valid data or not ******")
         
-        self.driver = setup
-        self.driver.get(self.baseURL)
+    #     self.driver = setup
+    #     self.driver.get(self.baseURL)
 
-        self.lp = loginPage(self.driver)
-        self.lp.setUsername(self.username)
-        self.lp.setPassword(self.password)
-        self.lp.clickLogin()
+    #     self.lp = loginPage(self.driver)
+    #     self.lp.setUsername(self.username)
+    #     self.lp.setPassword(self.password)
+    #     self.lp.clickLogin()
 
-        act_title = self.driver.title
-        if act_title == "Dashboard / nopCommerce administration":
-            assert True
-            self.driver.close()
-            self.logger.info("****** Login test passed ******")
+    #     act_title = self.driver.title
+    #     if act_title == "Dashboard / nopCommerce administration":
+    #         assert True
+    #         self.driver.close()
+    #         self.logger.info("****** Login test passed ******")
 
-        else:
-            self.driver.save_screenshot(".\\Screenshots\\" + "test_login.png")
-            self.driver.close()
-            self.logger.error("****** Login test failed ******")
-            assert False
+    #     else:
+    #         self.driver.save_screenshot(".\\Screenshots\\" + "test_login.png")
+    #         self.driver.close()
+    #         self.logger.error("****** Login test failed ******")
+    #         assert False
--- utilities/customLogger.py
+++ utilities/customLogger.py
@@ -6,7 +6,7 @@ class LogGenerator:
     #static methods are used so that we dont need to pass any parameter
     @staticmethod
     def log_generator():
-        logging.basicConfig(filename=".\\Logs\\automation.log",
+        logging.basicConfig(filename=".\\Logs\\automation.txt", filemode= "a",
                         format='%(asctime)s: %(levelname)s: %(message)s')
 
         logger = logging.getLogger()
