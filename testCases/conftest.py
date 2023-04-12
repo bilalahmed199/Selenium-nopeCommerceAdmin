ba7ee65165cab48026c2f97a1f29113375d4a9ba
Bilal Ahmed <bilalriasat199@gmail.com>
2023-04-12 11:53:23 +0500

    .

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -2,6 +2,23 @@ from selenium import webdriver
 import pytest
 
 @pytest.fixture()
-def setup():
-    driver = webdriver.Chrome()
+def setup(browser):
+    if browser == 'chrome':
+        driver = webdriver.Chrome()
+        print("Launching Chrome browser...")
+    elif browser=='firefox':
+        driver = webdriver.Firefox()
+        print("Launching Firefox browser...")
+    else: 
+        driver = webdriver.Ie()
     return driver
+
+# this will get value from CLI
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+
+# this will return Browser value to Setup function
+@pytest.fixture()
+def browser(request):
+    return request.config.getoption("--browser")
