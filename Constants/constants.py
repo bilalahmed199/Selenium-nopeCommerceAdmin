a623abfebaf313ac5dd0bdb2fef7e66d7d01c671
Bilal Ahmed <bilalriasat199@gmail.com>
2023-04-13 15:14:27 +0500

    Made constants.py file to save constants used in logs

---
+++ Constants/constants.py
@@ -0,0 +1,11 @@
+    
+#logs data, it will be saved in file
+test_suit_name = '****** Test_Suite_001_Login ******'
+test_case_1 = '****** TC1 - Verifying Login page loaded or not ******'
+test1_case_pass = "****** Pass - Login page loaded successfuly ******"
+test1_case_failed = "****** Fail - Login page not loaded ******" 
+
+test_case_2 = '****** TC2 - Verify user Login or not ******'
+test2_case_pass = "****** Pass - User Loggedin successfuly ******"
+test2_case_failed = "****** Fail - User Logedin failed ******" 
+end_line = '*******************************************\n'
