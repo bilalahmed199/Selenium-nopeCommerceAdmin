38544d1c34df7fdc71922d2490b09d353df27049
Bilal Ahmed <bilalriasat199@gmail.com>
2023-04-13 15:15:04 +0500

    Used Constants.py file instead of hard coded

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -2,6 +2,7 @@ import os
 from pageObjects.LoginPage import loginPage
 from utilities.readProperties import ReadConfig 
 from datetime import datetime
+from Constants import constants
 
 class Logger:
     def __init__(self, log_file_path):
@@ -20,37 +21,26 @@ class Test_001_Login:
     username = ReadConfig().getUsername()
     password = ReadConfig().getUserPassword()
 
-    #logs data, it will be saved in file
-    test_suit_name = '****** Test_Suite_001_Login ******'
-    test_case_1 = '****** TC1 - Verifying Login page loaded or not ******'
-    test1_case_pass = "****** Pass - Login page loaded successfuly ******"
-    test1_case_failed = "****** Fail - Login page not loaded ******" 
-
-    test_case_2 = '****** TC2 - Verify user Login or not ******'
-    test2_case_pass = "****** Pass - User Loggedin successfuly ******"
-    test2_case_failed = "****** Fail - User Logedin failed ******" 
-    end_line = '*******************************************\n'
-
     def test_loginPageLoaded(self,setup):
         self.driver = setup
         self.driver.get(self.baseURL)
         
-        logger.write_log(self.test_suit_name)
-        logger.write_log(self.test_case_1)
+        logger.write_log(constants.test_suit_name)
+        logger.write_log(constants.test_case_1)
 
         act_title = self.driver.title
         # it will fail as wrong title given, screenshot will be created
         if act_title == "Your store. Login":
             assert True
             self.driver.close()
-            logger.write_log(self.test1_case_pass)
-            logger.write_log(self.end_line)
+            logger.write_log(constants.test1_case_pass)
+            logger.write_log(constants.end_line)
 
         else:
             self.driver.save_screenshot(".\\Screenshots\\" + "test_homePageLoaded.png")
             self.driver.close()
-            logger.write_log(self.test1_case_failed)
-            logger.write_log(self.end_line)
+            logger.write_log(constants.test1_case_failed)
+            logger.write_log(constants.end_line)
             assert False
 
    
@@ -58,8 +48,8 @@ class Test_001_Login:
         self.driver = setup
         self.driver.get(self.baseURL)
         
-        logger.write_log(self.test_suit_name)
-        logger.write_log(self.test_case_2)
+        logger.write_log(constants.test_suit_name)
+        logger.write_log(constants.test_case_2)
 
         self.lp = loginPage(self.driver)
         self.lp.setUsername(self.username)
@@ -70,12 +60,12 @@ class Test_001_Login:
         if act_title == "Dashboard / nopCommerce administration":
             assert True
             self.driver.close()
-            logger.write_log(self.test2_case_pass)
-            logger.write_log(self.end_line)
+            logger.write_log(constants.test2_case_pass)
+            logger.write_log(constants.end_line)
 
         else:
             self.driver.save_screenshot(".\\Screenshots\\" + "test_login.png")
             self.driver.close()
-            self.logger.error(self.test2_case_failed)
-            logger.write_log(self.end_line)
+            self.logger.error(constants.test2_case_failed)
+            logger.write_log(constants.end_line)
             assert False
