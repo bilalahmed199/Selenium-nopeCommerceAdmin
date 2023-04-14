29c01665e3e00e962859c42c99299ef95b9686b7
Bilal Ahmed <bilalriasat199@gmail.com>
2023-04-14 10:00:51 +0500

    Fixed excel import by commenting out driverClose line

--- testCases/test_login_data_driven_testing.py
+++ testCases/test_login_data_driven_testing.py
@@ -1,10 +1,11 @@
 import os
 from pageObjects.LoginPage import loginPage
-from utilities.readProperties import ReadConfig 
+from utilities.readProperties import ReadConfig
 from datetime import datetime
 from utilities import excelReader
 import time
 from Constants import constants
+
 
 class Logger:
     def __init__(self, log_file_path):
@@ -15,8 +16,10 @@ class Logger:
         with open(self.log_file_path, 'a') as log_file:
             log_file.write(f'{current_time} - {log_message}\n')
 
+
 log_file_path = './Logs/automation.log'  # specify the path of the log file
 logger = Logger(log_file_path)
+
 
 # data driven testing
 # data is readed from Excel file
@@ -25,14 +28,14 @@ class Test_002_DDT_Login:
     # excel file path
     path = ".//testData/testData.xlsx"
 
-    #logs data, it will be saved in file
+    # logs data, it will be saved in file
     test_suit_name = '****** Test_Suite_002_Login ******'
     test_case_1 = '****** TC1 - Verifying user Loggedin with valid data or not ******'
     test1_case_pass = "****** Pass - User Loggedin successfuly ******"
-    test1_case_failed = "****** Fail - User Logedin failed ******" 
+    test1_case_failed = "****** Fail - User Logedin failed ******"
     end_line = '*******************************************\n'
 
-    def test_Login_DDT(self, setup ):
+    def test_Login_DDT(self, setup):
         self.driver = setup
         self.driver.get(self.baseURL)
         logger.write_log(self.test_suit_name)
@@ -40,16 +43,16 @@ class Test_002_DDT_Login:
 
         self.lp = loginPage(self.driver)
         # reading data from excel file
-        self.rows = excelReader.getRowCount(self.path, "Sheet1" )
+        self.rows = excelReader.getRowCount(self.path, "Sheet1")
         print("No. of rows in excel file: ", self.rows)
 
-        first_status = []   #empty list variable
+        first_status = []  # empty list variable
 
         # reading data from excel file
         for r in range(2, self.rows + 1):
-            self.user = excelReader.readData(self.path, "Sheet1",r,1)
-            self.passwrd = excelReader.readData(self.path, "Sheet1",r,2)
-            self.expectedResult = excelReader.readData(self.path, "Sheet1",r,3)
+            self.user = excelReader.readData(self.path, "Sheet1", r, 1)
+            self.passwrd = excelReader.readData(self.path, "Sheet1", r, 2)
+            self.expectedResult = excelReader.readData(self.path, "Sheet1", r, 3)
 
             self.lp.setUsername(self.user)
             self.lp.setPassword(self.passwrd)
@@ -69,7 +72,7 @@ class Test_002_DDT_Login:
                     logger.write_log(self.end_line)
                     self.lp.clickLogout()
                     first_status.append("Fail")
-            
+
             elif act_title != exp_title:
                 if self.expectedResult == "Pass":
                     logger.write_log(self.test1_case_failed)
@@ -80,14 +83,14 @@ class Test_002_DDT_Login:
                     logger.write_log(self.end_line)
                     first_status.append("Pass")
 
-        # checking the first_status list whether it meets our criteria or not
+        #checking the first_status list whether it meets our criteria or not
             if "Fail" not in first_status:
+                assert True
                 logger.write_log(self.test1_case_pass)
                 logger.write_log(self.end_line)
-                self.driver.close()
-                assert True
+                # self.driver.close()
             else:
                 logger.write_log(self.test1_case_failed)
                 logger.write_log(self.end_line)
-                self.driver.close()
+                # self.driver.close()
                 assert False
