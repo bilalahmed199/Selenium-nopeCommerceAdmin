0345fc790fc6fcd49d9c1bd5e2ed7bd3dfa9f9eb
Bilal Ahmed <bilalriasat199@gmail.com>
2023-04-15 15:09:29 +0500

    optimized the code by using variables

--- pageObjects/AddNewCustPage.py
+++ pageObjects/AddNewCustPage.py
@@ -9,7 +9,8 @@ class AddNewCustomer:
     txt_password_id = "Password"
     txt_firstName_id = "FirstName"
     txt_lastName_id = "LastName"
-    rd_gender_id = "Gender_Male"
+    rd_genderMale_id = "Gender_Male"
+    rd_genderFemale_id = "Gender_Female"
     txt_dob_id = "DateOfBirth"
     txt_company_id = "Company"
     rd_IsTaxExempt_xpath = "//input[@id='IsTaxExempt']"
@@ -27,68 +28,87 @@ class AddNewCustomer:
         self.driver = driver
 
     def clickOnCustomersMenu(self):
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.XPATH, self.linkCustomers_menu_xpath))).click()
-
+        customers_menu = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.XPATH, self.linkCustomers_menu_xpath)))
+        customers_menu.click()
+    
     def clickOnCustomersMenuItems(self):
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.XPATH, self.linkCustomers_menuItem_xpath))).click()
+        customers_menu_items = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.XPATH, self.linkCustomers_menuItem_xpath)))
+        customers_menu_items.click()
     
     def clickOnAddNew(self):
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.XPATH, self.btn_addNew_xpath))).click()
+        add_new_customer = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.XPATH, self.btn_addNew_xpath)))
+        add_new_customer.click()
 
     def enterEmail(self, email):
-        self.driver.find_element(By.ID, self.txt_email_id).clear()
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.id, self.txt_email_id))).send_keys(email)
+        enter_email = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.id, self.txt_email_id)))
+        enter_email.click()
+        enter_email.send_keys(email)
     
     def enterPassword(self, password):
-        self.driver.find_element(By.ID, self.txt_password_id).clear()
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.id, self.txt_password_id))).send_keys(password)
+        enter_password = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.id, self.txt_password_id)))
+        enter_password.send_keys(password)
 
     def enterFirstName(self, firstName):
-        self.driver.find_element(By.ID, self.txt_firstName_id).clear()
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.id, self.txt_firstName_id))).send_keys(firstName)
+        first_name = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.id, self.txt_firstName_id)))
+        first_name.clear()
+        first_name.send_keys(firstName)
 
     def enterLastName(self, lastName):
-        self.driver.find_element(By.ID, self.txt_lastName_id).clear()
+        last_name = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.id, self.txt_lastName_id)))
+        last_name.clear()
+        last_name.send_keys(lastName)
+
+    def selectGender(self, gender):
+        if gender == "Male":
+            element_id = self.rd_genderMale_id
+        elif gender == "Female":
+            element_id = self.rd_genderFemale_id
+        else:
+            element_id = self.rd_genderMale_id
+
         WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.id, self.txt_lastName_id))).send_keys(lastName)
-    
-    def selectGender(self):
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.ID, self.rd_gender_id))).click()
+        expected_conditions.presence_of_element_located((By.ID, element_id))).click()
+
 
     def enterDOB(self, dob):
-        self.driver.find_element(By.ID, self.txt_dob_id).clear()
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.ID, self.txt_dob_id))).send_keys(dob)
+        enter_DOB = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.ID, self.txt_dob_id)))
+        enter_DOB.click()
+        enter_DOB.send_keys(dob)
 
     def enterCompanyName(self, companyName):
-        self.driver.find_element(By.ID, self.txt_company_id).clear()
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.ID, self.txt_company_id))).send_keys(companyName)
+        company_name = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.ID, self.txt_company_id)))
+        company_name.clear()
+        company_name.send_keys(companyName)
   
    # click on newsletter list, search by text & select result
     def enterNewsletter(self, newsletter):
-        self.driver.find_element(By.XPATH, self.list_newsletter_xpath).clear()
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.XPATH, self.list_newsletter_xpath))).send_keys(newsletter)
+        enter_newsletter = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.XPATH, self.list_newsletter_xpath)))
+        enter_newsletter.clear()
+        enter_newsletter.send_keys(newsletter)
 
         WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.XPATH, self.select_newsletter_xpath))).click()
+        expected_conditions.element_to_be_clickable((By.XPATH, self.select_newsletter_xpath))).click()
    
     def enterCompanyName(self, companyName):
-        self.driver.find_element(By.ID, self.txt_company_id).clear()
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.ID, self.txt_company_id))).send_keys(companyName)
+        company_name = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.ID, self.txt_company_id)))
+        company_name.clear()
+        company_name.send_keys(companyName)
     
     def selectIsTaxExempt(self):
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.XPATH, self.rd_IsTaxExempt_xpath))).click()
+        is_tax_exampt = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.XPATH, self.rd_IsTaxExempt_xpath)))
+        is_tax_exampt.click()
 
     def selectRole(self, role):
         WebDriverWait(self.driver, 10).until(
@@ -115,9 +135,9 @@ class AddNewCustomer:
             self.listItem = WebDriverWait(self.driver, 10).until(
             expected_conditions.presence_of_element_located((By.XPATH, self.list_listItemVendors_xpath)))
 
-        else:
-            self.listItem = self.listItem = WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.XPATH, self.list_listItemGuests_xpath)))
+        # else:
+        #     self.listItem = self.listItem = WebDriverWait(self.driver, 10).until(
+        #     expected_conditions.presence_of_element_located((By.XPATH, self.list_listItemGuests_xpath)))
         # selecting the customer role
         # below line didn't worked, so will use execute_script line 
         # self.listItem.click()
